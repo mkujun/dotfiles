5f5518d2eff33af0c8cd4b313f30640d0da9768d
mkujun <kujundzic.marko@hotmail.com>
2023-01-30 20:12:55 +0100

    different color after column 80

--- windows/nvim/color.lua
+++ windows/nvim/color.lua
@@ -4,10 +4,8 @@ require("github-theme").setup({
 })
 
 -- different bg color after line 80
---[[
---
 vim.cmd[[
   set colorcolumn=80
   let &colorcolumn=join(range(81, 999),",")
+  highlight colorcolumn ctermbg=235 guibg=#161b22
 ]]
---]]
