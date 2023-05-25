c71102f508c818072443cd7e876a988ce0f56600
mkujun <kujundzic.marko@hotmail.com>
2023-05-25 21:42:12 +0200

    init.lua file browser

--- windows/nvim/init.lua
+++ windows/nvim/init.lua
@@ -9,27 +9,5 @@ require('nvim-autopairs').setup({-- this is required for nvim-autopairs to work
 })
 require("lsp") -- lsp configuration
 require("completion") -- nvim-cmp config
-require("formatting")
-
--- You don't need to set any of these options.
--- IMPORTANT!: this is only a showcase of how you can set default options!
-require("telescope").setup {
-  extensions = {
-    file_browser = {
-      theme = "ivy",
-      -- disables netrw and use telescope-file-browser in its place
-      hijack_netrw = true,
-      mappings = {
-        ["i"] = {
-          -- your custom insert mode mappings
-        },
-        ["n"] = {
-          -- your custom normal mode mappings
-        },
-      },
-    },
-  },
-}
--- To get telescope-file-browser loaded and working with telescope,
--- you need to call load_extension, somewhere after setup function:
-require("telescope").load_extension "file_browser"
+require("formatting") -- prettier config with null ls
+require("file_browser") -- file browser setup
