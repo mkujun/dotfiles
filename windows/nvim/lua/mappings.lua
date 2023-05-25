e1bd332501ff10ea25437e050fa6285a0a3be7d4
mkujun <kujundzic.marko@hotmail.com>
2023-05-25 21:38:28 +0200

    separate file for file formatting

--- windows/nvim/lua/mappings.lua
+++ windows/nvim/lua/mappings.lua
@@ -26,9 +26,9 @@ vim.api.nvim_set_keymap(
 )
 
 -- open file_browser with the path of the current buffer
-vim.api.nvim_set_keymap(
-  "n",
-  "<space>fb",
-  ":Telescope file_browser path=%:p:h select_buffer=true",
-  { noremap = true }
-)
+-- vim.api.nvim_set_keymap(
+  -- "n",
+  -- "<space>fb",
+  -- ":Telescope file_browser path=%:p:h select_buffer=true",
+  -- { noremap = true }
+-- )
