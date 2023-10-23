74e549be96d0c4f492cfed8b9377d200cfee0025
mkujun <kujundzic.marko@hotmail.com>
2023-10-23 09:42:05 +0200

    removed references of telescope file browser

--- windows/nvim/init.lua
+++ windows/nvim/init.lua
@@ -1,3 +1,7 @@
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 require('impatient')
 require("plugins")
 require("settings")
@@ -32,15 +36,8 @@ require("telescope").setup {
 }
 
 
--- disable netrw at the very start of your init.lua
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
-
--- empty setup using defaults
-require("nvim-tree").setup()
 
 -- OR setup with some options
 require("nvim-tree").setup({
--- windows/nvim/lua/settings.lua
+++ windows/nvim/lua/settings.lua
@@ -33,7 +33,3 @@ function _G.custom_fold_text()
 end
 
 vim.diagnostic.config({virtual_text = false}) -- disable inline virtual text error for lsp
-
--- disable netrw, nvim-tree
---vim.g.loaded_netrw = 1
---vim.g.loaded_netrwPlugin = 1
